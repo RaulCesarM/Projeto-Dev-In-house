f8282c41ed72546365238de2b1799dceb067bb53
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-04-11 20:41:05 -0300

    fixando valor

--- Src/index.js
+++ Src/index.js
@@ -1,16 +1,18 @@
 const Produto = document.querySelector("input#Listar1");
 const Tamanho = document.querySelector("input#Listar2");
 const Preco = document.querySelector("input#Listar3");
-const Botao_Adicionar = document.querySelector("form button");
+const Botao_Adicionar = document.querySelector("form button#BTN_adicionar");
+const Botao_Listar = document.querySelector("form button#BTN_listar");
 const UL_lista = document.querySelector("ul1");
 
-var soma = 0;
 
-Botao_Adicionar.onclick = (ev) => {
-  ev.preventDefault();
+
+Botao_Adicionar.onclick = (evento) => {
+  evento.preventDefault();
 
   if (Produto.value) {
     salvarProduto();
+   
 
     Produto.value = "";
     Tamanho.value = "";
@@ -18,7 +20,13 @@ Botao_Adicionar.onclick = (ev) => {
   }
 };
 
+Botao_Listar.onclick = (evento) => {
+  evento.preventDefault();
+  carregarProdutos();
+};
+
 function salvarProduto() {
+  
   var produto = {
     nome: Produto.value,
     tamanho: Tamanho.value,
@@ -30,105 +38,88 @@ function salvarProduto() {
   }
   produtos.push(produto);
   localStorage.setItem("produtos", JSON.stringify(produtos));
+  
   carregarProdutos();
 }
-
-
 
 function carregarProdutos() {
   var produtos = [];
 
-  if (localStorage.getItem("produtos")) {
+  if (localStorage.getItem("produtos")) {//verificase tem 
     produtos = JSON.parse(localStorage.getItem("produtos"));
   }
   UL_lista.innerHTML = "";
-
+  
   produtos.forEach((produto) => {
     const Valor = document.createElement("span");
-    Valor.innerHTML = produto.nome + "| R$ " + produto.preco;
-
     const Botao_excluir = document.createElement("button");
-    //const Input_Check = document.createElement("input");
+    const checkbox = document.createElement("input");
+    const LI_lista = document.createElement("li"); 
+
+    Valor.innerHTML = produto.nome + "| R$ " + produto.preco;
     Botao_excluir.innerHTML = "remover";
-    const LI_lista = document.createElement("li");
+    checkbox.type = "checkbox";
+
     LI_lista.appendChild(Valor);
     LI_lista.appendChild(Botao_excluir);
-  //  LI_lista.appendChild(Input_Check);
+    LI_lista.appendChild(checkbox);
+
+
 
     let nomedefora = produto.nome;
 
     Botao_excluir.onclick = () => {
       if (confirm("Deseja remover o produto?")) {
         removerProdutopornome(nomedefora);
-        UL_lista.removeChild(LI_lista);
+        UL_lista.removeChild(LI_lista); 
+        exibirsoma()      
         alert("Produto removido com sucesso!");
       } else {
         alert("Produto não removido");
+       
       }
     };
 
     LI_lista.appendChild(Valor);
     UL_lista.appendChild(LI_lista);
+    
   });
-
- // localStorage.setItem("produtos", JSON.stringify(produtos));
+  
+  exibirsoma()
 }
 
-
-
-// const sacar = (id, valor) => {
-//   let Cliente = contas.find((IdConta) => IdConta.id === id);
-//   let ValorSaque = valor;
-//   let SaldoAtual = Cliente.saldo;
-
-//   if (ValorSaque <= 0) {
-//     console.log(
-//       "valor de R$: " +
-//         ValorSaque +
-//         "é inválido e não realizar nenhuma operação"
-//     );
-//   } else if (ValorSaque > SaldoAtual) {
-//     console.log(
-//       "mensagem de saldo insuficiente seu saldo é R$:" + SaldoCliente(id)
-//     );
-//   } else if (ValorSaque > 0 && ValorSaque <= SaldoAtual) {
-//     Cliente.saldo -= ValorSaque;
-//     console.log("Movimentação na conta de ID nº:" +id);
-//     console.log(
-//       "o saque de R$: " +
-//         ValorSaque +
-//         "  ocorreu com sucesso e o saldo atual da conta após o saque de R$: " +
-//         SaldoCliente(id) +
-//         " !"
-//     );
-//   } else {
-//     return "";
-//   }
-// };
-
-
 function removerProdutopornome(nome) {
-
-  
   var produtos = [];
   if (localStorage.getItem("produtos")) {
     produtos = JSON.parse(localStorage.getItem("produtos"));
   }
-  var index = produtos.indexOf(nome);
-  console.log("index na função" + index);
-  
-  let adeletar = produtos.filter((nomeProd) => nomeProd.nome === nome);
-  //produtos.pop(adeletar);
-  console.log(adeletar)
-  console.log([{adeletar}])
-  // produtos.forEach((produto) => {
-  //   if (produto.nome === nome) {
-  //     console.log("nome prduto" + produto.nome);
-  //     console.log("nome na funcao deletar" + nome);
-      
-  //     produtos.pop(produto.nome);
-  //   }
-  // });
 
-  localStorage.setItem("produtos", JSON.stringify(produtos));
+  let prodtosfiltrados = produtos.filter((nomeProd) => nomeProd.nome !== nome);
+
+  localStorage.setItem("produtos", JSON.stringify(prodtosfiltrados));
+  somarPrecos()
 }
+
+function somarPrecos() {
+  var soma =0;
+  var produtos = [];
+  if (localStorage.getItem("produtos")) {
+    produtos = JSON.parse(localStorage.getItem("produtos"));
+  }
+  produtos.forEach((produto) => {
+    soma += parseFloat(produto.preco);
+  });
+  return soma;
+}
+
+///removeChild()
+
+function exibirsoma() {
+  var soma = somarPrecos(); 
+  const mostrar = document.createElement("p");
+  mostrar.innerHTML = `<p>total a pagar ${soma} </p>`;
+  document.getElementById("totalapagar").replaceChild(mostrar, document.getElementById("totalapagar").childNodes[0]);
+  //document.getElementById("totalapagar").appendChild(mostrar);
+  
+}
+
